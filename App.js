4980002d0bdb4f62867ba25cb3b5ff754304613f
DimickTravel <dimick.travel@gmail.com>
2025-06-21 18:26:01 +1000

    Update and rename App.js to public/index.html

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HomePage from ".src/HomePage"; // make sure this matches your file name and location
-
-function App() {
-  return <HomePage />;
-}
-
-export default App;
